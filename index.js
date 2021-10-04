b9d01d74886a22b4505519ffb5285894a6c2d24b
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-04 14:08:09 +0000

    CC-03 completed

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 //1
-function countToTen(){
-  
+function countFromOne(number){
+  for(let i = 1; i <= number; i++) {
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countEveryOdd(number){
+  for(let i = 1; i <= number; i += 1) {
+    if (i % 2 !== 0) {
+       console.log(i);
+      }
+  }
 }
 
 //3
-function countEveryEven(){
-  
+function isNegative(number){
+  if (number < 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //4
-function countEveryOdd(){
+function betweenFiveAndTwenty(integer){
+  if (5 <= integer || integer <= 20) {
+    return true;
+  } else {
+    return false;
+  }
   
 }
+// }
 
 //5 
-function countEvens(){
-  
+function isAllLowerCase(str){
+  if (str === str.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+// Call
+countFromOne(20);
+countEveryOdd(15);
+console.log(isNegative(-5));
+console.log(betweenFiveAndTwenty(100));
+console.log(isAllLowerCase("heLlo"));
